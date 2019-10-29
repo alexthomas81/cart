602ca34e40595f0d0ab0d7ea858ee20e33b57dd7
Alex Thomas <alexthomas81@gmail.com>
2019-10-29 22:24:16 +0530

    Update Cart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import data from './dataList';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Products from './components/Products';
+import CartDetails from './components/cartDetails';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Class Component: - Smart/ Container component
+class App extends Component {
+  state = {
+    productList: data,
+    cartList: [],
+  };
+
+  addItemToCart = (item) => {
+    console.log(' added item =', item);
+    const { cartList } = this.state;
+    const newCartDetails = [...cartList];
+    newCartDetails.push(item);
+    console.log('New Added Items = ', newCartDetails);
+    this.setState({
+      cartList: newCartDetails,
+    });
+  }
+
+  render() {
+    const { productList, cartList } = this.state;
+    return (
+      <div className="App">
+        <Header msg="React Shopping Cart!" className="head_container">
+          <h5>Amazing shoes here !! </h5>
+        </Header>
+        <Router>
+          <nav>
+            <Link to='/'>Products</Link>
+            <Link to='/about'>CartDetails</Link>
+          </nav>
+          <Switch >
+            <Route exact path="/" >
+              <Products
+                allProducts={productList}
+                addToCart={this.addItemToCart}
+              />
+            </Route>
+            <Route exact path="/about" ><CartDetails cartItems={cartList} /></Route>
+          </Switch>
+        </Router>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
